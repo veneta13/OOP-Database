49d33e78dfa6bf28d36137211edfe50dc7af7ca1
Veneta <venkireva80@gmail.com>
2022-06-06 22:08:40 +0300

    Add StringValue class and documentation for it

---
+++ source/string_value.cpp
@@ -0,0 +1,93 @@
+#include "string_value.hpp"
+
+/// Default constructor
+StringValue::StringValue() {
+    value = nullptr;
+}
+
+
+/// Constructor with parameters
+StringValue::StringValue(char const *_value) {
+    value = new char [strlen(_value) + 1];
+    strcpy(value, _value);
+}
+
+
+/// Copy constructor
+/// \param other object to copy
+StringValue::StringValue(StringValue const &other) {
+    value = new char [strlen(other.value) + 1];
+    strcpy(value, other.value);
+}
+
+
+/// Copy assignment operator
+/// \param other object to copy
+/// \return updated object
+StringValue& StringValue::operator=(StringValue const &other) {
+    if (this != &other) {
+        delete[] value;
+        value = new char [strlen(other.value) + 1];
+        strcpy(value, other.value);
+    }
+    return *this;
+}
+
+
+/// Destructor
+StringValue::~StringValue() {
+    delete[] value;
+    value = nullptr;
+}
+
+
+/// Value getter
+/// \return value
+char *StringValue::get() const {
+    return value;
+}
+
+
+/// Value setter
+/// \param _value the updated value of value
+void StringValue::set(char const *_value) {
+    delete[] value;
+    value = new char [strlen(_value) + 1];
+    strcpy(value, _value);
+}
+
+
+/// Compare with equality operator
+/// \param other StringValue to compare to
+/// \return if the current value is equal to the other
+bool StringValue::operator==(StringValue const &other) const {
+    if (!value || !other.value) { return false; }
+    return (strcmp(value, other.value) == 0);
+}
+
+
+/// Compare with inequality operator
+/// \param other StringValue to compare to
+/// \return if the current value is not equal to the other
+bool StringValue::operator!=(StringValue const &other) const {
+    if (!value || !other.value) { return false; }
+    return (strcmp(value, other.value) != 0);
+}
+
+
+/// Compare with equality operator
+/// \param other string to compare to
+/// \return if the current value is equal to the other
+bool StringValue::operator==(char const *other) const {
+    if (!value || !other) { return false; }
+    return (strcmp(value, other) == 0);
+}
+
+
+/// Compare with equality operator
+/// \param other string to compare to
+/// \return if the current value is not equal to the other
+bool StringValue::operator!=(char const *other) const {
+    if (!value || !other) { return false; }
+    return (strcmp(value, other) != 0);
+}
---
+++ source/string_value.hpp
@@ -0,0 +1,29 @@
+#ifndef STACK_STRING_VALUE_HPP
+#define STACK_STRING_VALUE_HPP
+
+#include "value.hpp"
+#include <cstring>
+
+/// Represents the value of a table cell with an integer
+class StringValue : public Value {
+    /// The value in the cell
+    char* value = nullptr;
+
+public:
+    StringValue();
+    StringValue(const char* _value);
+    StringValue(StringValue const& other);
+    StringValue& operator=(StringValue const& other);
+    ~StringValue() override;
+
+    bool operator==(StringValue const& other) const;
+    bool operator!=(StringValue const& other) const;
+
+    bool operator==(char const* other) const;
+    bool operator!=(char const* other) const;
+
+    char* get() const;
+    void set(char const* _value);
+};
+
+#endif
